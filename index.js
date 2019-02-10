db8819fdd0f673508b1f46d7595ed5bc73bcbbd0
Kazuma Matsudo <matsudo.kazuma@unipro.co.jp>
2019-02-10 16:19:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const server = require("express")();
 const line = require("@line/bot-sdk");
 
-const line_config = {
+const lineConfig = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN,
     channelSecret: process.env.LINE_CHANNEL_SECRET
 };
+const bot = new line.Client(lineConfig);
 
 server.post(
     '/webhook',
-    line.middleware(line_config),
+    line.middleware(lineConfig),
     (req, res) => {
         res.sendStatus(200);
-        console.log(req.body);
+
+        const eventsProcessed = [];
+        req.body.events.forEach(event => {
+            if (event.type === "message" && event.message.type === "text"){
+                eventsProcessed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: `あなたは 「${event.message.text}」 と入力しました。`
+                }));
+            }
+        });
+
+        Promise
+            .all(eventsProcessed)
+            .then(response => console.log(`${response.length} event(s) processed.`));
     });
 
 server.listen(process.env.PORT || 3000);
